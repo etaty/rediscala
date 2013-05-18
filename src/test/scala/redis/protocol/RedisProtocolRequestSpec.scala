57f1502b5ba6b356b40515ecdb7a1d55850f8066
Valerian Barbot <vbr@zenexity.com>
2013-05-18 10:43:09 +0200

    add test for RedisProtocolRequest

---
+++ src/test/scala/redis/protocol/RedisProtocolRequestSpec.scala
@@ -0,0 +1,18 @@
+package redis.protocol
+
+import akka.util.ByteString
+import org.specs2.mutable._
+
+class RedisProtocolRequestSpec extends Specification {
+
+  "Encode request" should {
+    "inline" in {
+      RedisProtocolRequest.inline("PING") mustEqual ByteString("PING\r\n")
+    }
+    "multibulk" in {
+      val encoded = RedisProtocolRequest.multiBulk("SET", Seq(ByteString("mykey"), ByteString("myvalue")))
+      encoded mustEqual ByteString("*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n")
+    }
+  }
+
+}
