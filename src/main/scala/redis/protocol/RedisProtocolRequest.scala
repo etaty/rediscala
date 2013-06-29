f654b6fc99eab94a0a2f77ff490a6710eb9bf184
Valerian Barbot <vbr@zenexity.com>
2013-06-29 20:00:16 +0200

    faster request encoding

--- src/main/scala/redis/protocol/RedisProtocolRequest.scala
+++ src/main/scala/redis/protocol/RedisProtocolRequest.scala
@@ -1,27 +1,35 @@
 package redis.protocol
 
 import akka.util.ByteString
+import java.nio.charset.Charset
 
 object RedisProtocolRequest {
+  val UTF8_CHARSET = Charset.forName("UTF-8")
   val LS_STRING = "\r\n"
-  val LS = LS_STRING.getBytes("UTF-8")
+  val LS = LS_STRING.getBytes(UTF8_CHARSET)
+
 
   def multiBulk(command: String, args: Seq[ByteString]): ByteString = {
     val requestBuilder = ByteString.newBuilder
-    requestBuilder.putBytes((s"*${args.size + 1}").getBytes("UTF-8"))
+    requestBuilder.putByte('*')
+    requestBuilder.putBytes((args.size + 1).toString.getBytes(UTF8_CHARSET))
     requestBuilder.putBytes(LS)
 
-    val builder = (ByteString(command.getBytes("UTF-8")) +: args).foldLeft(requestBuilder) {
-      case (acc, arg) =>
-        acc.putBytes((s"$$${arg.size}").getBytes("UTF-8"))
-        acc.putBytes(LS)
-        acc ++= (arg)
-        acc.putBytes(LS)
+    requestBuilder.putByte('$')
+    requestBuilder.putBytes(command.length.toString.getBytes(UTF8_CHARSET))
+    requestBuilder.putBytes(LS)
+    requestBuilder.putBytes(command.getBytes(UTF8_CHARSET))
+    requestBuilder.putBytes(LS)
 
-        acc
-    }
+    args.foreach(arg => {
+      requestBuilder.putByte('$')
+      requestBuilder.putBytes(arg.length.toString.getBytes(UTF8_CHARSET))
+      requestBuilder.putBytes(LS)
+      requestBuilder ++= arg
+      requestBuilder.putBytes(LS)
+    })
 
-    builder.result()
+    requestBuilder.result()
   }
 
   def inline(command: String): ByteString = ByteString(command + LS_STRING)
