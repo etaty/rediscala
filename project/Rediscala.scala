6af8d804abf54c42dfffc70bdc74145d6667d54f
Valerian Barbot <vbr@zenexity.com>
2014-10-31 11:59:18 +0100

    1.4.2

--- project/Rediscala.scala
+++ project/Rediscala.scala
@@ -39,7 +39,7 @@ object Dependencies {
 object RediscalaBuild extends Build {
   val baseSourceUrl = "https://github.com/etaty/rediscala/tree/"
 
-  val v = "1.4.1"
+  val v = "1.4.2"
 
   lazy val standardSettings = Defaults.defaultSettings ++
     Seq(
