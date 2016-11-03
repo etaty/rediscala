b8725fb023233953c105c7cfc7251edcef6f2a55
Nicolas Peters <peters.nico@gmail.com>
2016-11-03 01:19:43 +0100

    improve pattern matching

--- src/main/scala/redis/api/Clusters.scala
+++ src/main/scala/redis/api/Clusters.scala
@@ -21,42 +21,40 @@ case class ClusterSlots() extends RedisCommand[MultiBulk,Seq[ClusterSlot]] {
   val isMasterOnly = false
   val encodedRequest: ByteString = encode("CLUSTER SLOTS")
 
-  def buildClusterNode(vect:Seq[RedisReply]) = {
-    ClusterNode(vect(0).toByteString.utf8String,vect(1).toByteString.utf8String.toInt,vect(2).toByteString.utf8String  )
+  def buildClusterNode(vect:Seq[RedisReply]): ClusterNode = {
+    ClusterNode(vect(0).toByteString.utf8String,vect(1).toByteString.utf8String.toInt,vect(2).toByteString.utf8String)
   }
 
-  def decodeReply(mb: MultiBulk) = {
-    val clusterSlots: Option[Seq[Option[ClusterSlot]]] = mb.responses.map{ vector =>
-      val seq: Seq[RedisReply] = vector.toSeq
-      seq.collect { case multiBulk:MultiBulk =>
+  def decodeReply(mb: MultiBulk): Seq[ClusterSlot] = {
+    val clusterSlots: Option[Seq[ClusterSlot]] = mb.responses.map{ vector =>
+      vector.collect {
+        case MultiBulk(Some(groupSlot)) =>
         //
         // redis response:
         // MultiBulk(begin,end,MultiBulk(masterId,masterPort,masterId),MultiBulk(slave1Id,slave1Port,slave1Id),MultiBulk(slave2Id,slave2Port,slave2Id))...,
         // MultiBulk(begin,end,MultiBulk(masterId,masterPort,masterId),MultiBulk(slave1Id,slave1Port,slave1Id),MultiBulk(slave2Id,slave2Port,slave2Id))
         //
-        multiBulk.responses.map { groupSlot =>
+        val begin = groupSlot(0).toByteString.utf8String.toInt
+        val end = groupSlot(1).toByteString.utf8String.toInt
+        val masterMB = groupSlot(2)
 
-          val begin = groupSlot(0).toByteString.utf8String.toInt
-          val end = groupSlot(1).toByteString.utf8String.toInt
-          val masterMB = groupSlot(2).asInstanceOf[MultiBulk]
-
-         val masterNode = masterMB.responses.map(
-            vect =>  buildClusterNode(vect.toSeq)
-          ).getOrElse(throw new RuntimeException("no master found"))
-
-          val slavesNode: Seq[ClusterNode] = groupSlot.lift(3).flatMap { slavesMBTmp =>
-            slavesMBTmp.asInstanceOf[MultiBulk]
-                .responses.map {
-                _.grouped(3).map { vect =>
-                  buildClusterNode(vect.toSeq)
-                }.toSeq
-              }
-          }.getOrElse(Seq.empty)
-          ClusterSlot(begin,end,masterNode,slavesNode)
+        val masterNode = masterMB match {
+          case MultiBulk(Some(vect)) => buildClusterNode(vect)
+          case _ => throw new RuntimeException("no master found")
         }
+
+        val slavesNode: Seq[ClusterNode] = groupSlot.lift(3).map {
+          case MultiBulk(Some(responses)) =>
+              responses.grouped(3).map { vect =>
+                buildClusterNode(vect)
+              }.toSeq
+          case _ => Seq.empty
+        }.getOrElse(Seq.empty)
+        ClusterSlot(begin,end,masterNode,slavesNode)
+
       }
     }
-    clusterSlots.getOrElse(Seq.empty).flatten
+    clusterSlots.getOrElse(Seq.empty)
   }
 
   override val decodeRedisReply: PartialFunction[ByteString, DecodeResult[MultiBulk]] = {
--- src/test/scala/redis/RedisClusterTest.scala
+++ src/test/scala/redis/RedisClusterTest.scala
@@ -2,8 +2,9 @@ package redis
 
 
 import akka.util.ByteString
+import org.specs2.execute.Result
 import redis.api.clusters.ClusterSlots
-import redis.protocol.{DecodeResult, MultiBulk, RedisProtocolReply, RedisReply}
+import redis.protocol._
 
 import scala.concurrent.Await
 
@@ -38,15 +39,22 @@ class RedisClusterTest extends RedisClusterClients {
       val clusterSlotsAsByteString = ByteString(new sun.misc.BASE64Decoder().decodeBuffer("KjMNCio0DQo6MA0KOjU0NjANCiozDQokOQ0KMTI3LjAuMC4xDQo6NzAwMA0KJDQwDQplNDM1OTlkZmY2ZTNhN2I5ZWQ1M2IxY2EwZGI0YmQwMDlhODUwYmE1DQoqMw0KJDkNCjEyNy4wLjAuMQ0KOjcwMDMNCiQ0MA0KYzBmNmYzOWI2NDg4MTVhMTllNDlkYzQ1MzZkMmExM2IxNDdhOWY1MA0KKjQNCjoxMDkyMw0KOjE2MzgzDQoqMw0KJDkNCjEyNy4wLjAuMQ0KOjcwMDINCiQ0MA0KNDhkMzcxMjBmMjEzNTc4Y2IxZWFjMzhlNWYyYmY1ODlkY2RhNGEwYg0KKjMNCiQ5DQoxMjcuMC4wLjENCjo3MDA1DQokNDANCjE0Zjc2OWVlNmU1YWY2MmZiMTc5NjZlZDRlZWRmMTIxOWNjYjE1OTINCio0DQo6NTQ2MQ0KOjEwOTIyDQoqMw0KJDkNCjEyNy4wLjAuMQ0KOjcwMDENCiQ0MA0KYzhlYzM5MmMyMjY5NGQ1ODlhNjRhMjA5OTliNGRkNWNiNDBlNDIwMQ0KKjMNCiQ5DQoxMjcuMC4wLjENCjo3MDA0DQokNDANCmVmYThmZDc0MDQxYTNhOGQ3YWYyNWY3MDkwM2I5ZTFmNGMwNjRhMjENCg=="))
       val clusterSlotsAsBulk: DecodeResult[RedisReply] = RedisProtocolReply.decodeReply(clusterSlotsAsByteString)
       var decodeValue = ""
-      clusterSlotsAsBulk.map({
+      val dr: DecodeResult[String] = clusterSlotsAsBulk.map({
         case a: MultiBulk =>
-          decodeValue = ClusterSlots().decodeReply(a).toString()
-          case _ => failure
+          ClusterSlots().decodeReply(a).toString()
+        case _ => "fail"
       })
 
-      decodeValue mustEqual "Vector(ClusterSlot(0,5460,ClusterNode(127.0.0.1,7000,e43599dff6e3a7b9ed53b1ca0db4bd009a850ba5),Stream(ClusterNode(127.0.0.1,7003,c0f6f39b648815a19e49dc4536d2a13b147a9f50), ?)), " +
-        "ClusterSlot(10923,16383,ClusterNode(127.0.0.1,7002,48d37120f213578cb1eac38e5f2bf589dcda4a0b),Stream(ClusterNode(127.0.0.1,7005,14f769ee6e5af62fb17966ed4eedf1219ccb1592), ?)), " +
-        "ClusterSlot(5461,10922,ClusterNode(127.0.0.1,7001,c8ec392c22694d589a64a20999b4dd5cb40e4201),Stream(ClusterNode(127.0.0.1,7004,efa8fd74041a3a8d7af25f70903b9e1f4c064a21), ?)))"
+
+      val r: Result = dr match {
+        case FullyDecoded(decodeValue, _) => decodeValue mustEqual "Vector(ClusterSlot(0,5460,ClusterNode(127.0.0.1,7000,e43599dff6e3a7b9ed53b1ca0db4bd009a850ba5),Stream(ClusterNode(127.0.0.1,7003,c0f6f39b648815a19e49dc4536d2a13b147a9f50), ?)), " +
+          "ClusterSlot(10923,16383,ClusterNode(127.0.0.1,7002,48d37120f213578cb1eac38e5f2bf589dcda4a0b),Stream(ClusterNode(127.0.0.1,7005,14f769ee6e5af62fb17966ed4eedf1219ccb1592), ?)), " +
+          "ClusterSlot(5461,10922,ClusterNode(127.0.0.1,7001,c8ec392c22694d589a64a20999b4dd5cb40e4201),Stream(ClusterNode(127.0.0.1,7004,efa8fd74041a3a8d7af25f70903b9e1f4c064a21), ?)))"
+
+        case _ => failure
+      }
+
+      r
     }
 
   }
