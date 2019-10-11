bbb96232b4ae392fe23179abfe11924946ad42dc
David Montgomery <david.montgomery@topgolf.com>
2019-10-10 17:17:58 -0700

    Update install script to redis-4.0.14.

--- install-redis.sh
+++ install-redis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-wget http://download.redis.io/releases/redis-3.2.0.tar.gz
-tar -xzvf redis-3.2.0.tar.gz
-cd redis-3.2.0 && make
+wget http://download.redis.io/releases/redis-4.0.14.tar.gz
+tar -xzvf redis-4.0.14.tar.gz
+cd redis-4.0.14 && make
